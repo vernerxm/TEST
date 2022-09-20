110ac02b30eebcd8aeaae81a80baaac8d83a90df
vernerxm <maksderiy1@gmail.com>
2022-09-20 12:21:42 +0300

    0.0.1.4
    grid

--- TEST/Assets/LolScrept.cs
+++ TEST/Assets/LolScrept.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class LolScrept : MonoBehaviour
 {
+
     public void Foo() => Debug.Log("Changes");
+
+
+    void Start()
+    {
+        Debug.Log(1);
+    }
+
+    
+    void Update()
+    {
+        
+    }
 
 }
